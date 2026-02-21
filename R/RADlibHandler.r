a846e8ac0ebe1a7d2dda8c8ff14ff59f3263e297
rachelseamons <rachelseamonshcp@gmail.com>
2026-02-21 13:45:34 -0700

    code for reading fasta files restructured to be a function

--- R/RADlibHandler.r
+++ R/RADlibHandler.r
@@ -1,15 +1,35 @@
 library(Biostrings)
 
-infile <- "C:/Users/rache/OneDrive/Desktop/Capstone/RADalign/inst/extdata/RADlibV.fa"
+radv_file <- "C:/Users/rache/OneDrive/Desktop/Capstone/RADalign/inst/extdata/RADlibV.fa"
 
 # vector of desired accession numbers
 wanted <- c("GCF_000006765.1.1")
 
-# filter the indexes of the sequences that contain the accession numbers
-# from the fasta.index dataframe that contains summary information about
-# the database file. Names are stored in the "desc" column of the dataframe.
-fastaindex <- fasta.index(infile, seqtype = "DNA")
-wanted_index <- grepl(paste(wanted), fastaindex$desc)
-subset_index <- fastaindex[wanted_index, ]
-filtered <- readDNAStringSet(subset_index)
-print(filtered)
+#' readSequences
+#'
+#' Read in all sequences that include the given accession numbers in their
+#' identification line.
+#'
+#' @param infile The file path to the desired library
+#' @param accessions A list of accession numbers to read in
+#'
+#' @return A Biostrings DNAStringSet containing all sequences that had one
+#' of the given accession numbers in their identification line
+#'
+#' @export
+#'
+#' @examples
+#' readSequences("/my/file/path", c("1234.1", "1234.2", "1234.3"))
+readSequences <- function(infile, accessions) {
+    # create summary dataframe for database file
+    fastaindex <- fasta.index(infile, seqtype = "DNA")
+
+    # filter the indexes of the sequences that contain the accession numbers
+    # names are stored in the "desc" column of the dataframe
+    wanted_index <- grepl(paste(accessions), fastaindex$desc)
+
+    # use matching indexes to read only desired sequences
+    subset_index <- fastaindex[wanted_index, ]
+    filtered <- readDNAStringSet(subset_index)
+    return(filtered)
+}
