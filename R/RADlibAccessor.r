e7306eff0f1816daca4a059a980669f0e236afb5
rachelseamons <rachelseamonshcp@gmail.com>
2026-02-20 20:14:59 -0700

    Updated database handler class to be temporarily unused

--- R/RADlibAccessor.r
+++ R/RADlibAccessor.r
@@ -1,25 +1,28 @@
-library(Biostrings)
+# ON PAUSE AWAITING FEEDBACK
+# library(Biostrings)
 
-setClass(
-    "RADlibAccessor",
-    slots = list(
-        radv = "list"
-    )
-    # TODO::add validity check or prototype (default constructor) if needed
-)
+# setClass(
+#     "RADlibAccessor",
+#     slots = list(
+#         radv = "list"
+#     )
+#     # TODO::add validity check or prototype (default constructor) if needed
+# )
 
-setGeneric("get_v_regions", function(object) standardGeneric("get_v_regions"))
-setGeneric("createRADv", function(object) standardGeneric("createRADv")) # TODO:: need better name for this, maybe "readDatabase"?
+# setGeneric("get_v_regions", function(object, vregions) standardGeneric("get_v_regions"))
+# setGeneric("createRADv", function(object) standardGeneric("createRADv")) # TODO:: need better name for this, maybe "readDatabase"?
 
-setMethod("get_v_regions", "RADv", function(object) {
-    # TODO:: figure out how to specify parameters so you can specify which vregion you're looking for
-    sequences <- radv[grep("vregion", names(radv), value = TRUE)]
+# setMethod("get_v_regions", "RADv", function(object, vregions) {
+#     # TODO:: figure out how to specify parameters so you can specify which vregion you're looking for
+#     print("successfully called get_v_regions")
 
-    sequenceSet <- NULL
-    for (sequence in sequences) {
-        ID <- # TODO:: get ID by getting name of sequence; name = ID (see radv brainstorming doc)
-            sequenceSet <- c(sequenceSet, ID = ) # TODO:: convert sequence to Biostring
-    }
-})
+#     # sequences <- radv[grep("vregion", names(radv), value = TRUE)]
 
-setMethod("createRADv", "RADv", function(object) {})
+#     # sequenceSet <- NULL
+#     # for (sequence in sequences) {
+#     #     ID <- # TODO:: get ID by getting name of sequence; name = ID (see radv brainstorming doc)
+#     #         sequenceSet <- c(sequenceSet, ID = ) # TODO:: convert sequence to Biostring
+#     # }
+# })
+
+# setMethod("createRADv", "RADv", function(object) {})
