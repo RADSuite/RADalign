537a92bc5c921df07690ad99524e2bc2836ff6be
rachelseamons <rachelseamonshcp@gmail.com>
2026-02-26 10:37:03 -0700

    successfully separating groups of identical sequences and assigning IDs

--- R/RADalign.R
+++ R/RADalign.R
@@ -28,27 +28,23 @@ selectTaxa <- function(taxa) {
 alignVRegions <- function(sequences) {
     sequences <- readSequences(RADlibV, c("GCF_000006765.1"))
 
+    IDs <- list()
     for (region in all_v_regions) {
+        # get all sequences for region and perform msa
         region_sequences <- getVRegions(sequences, region)
         alignment <- msa(region_sequences, method = "ClustalOmega")
 
-        # use phangorn's ML-based distance calculation to determine
-        # which sequences are identical
-        phy <- phyDat(as.matrix(alignment), type = "DNA")
-        distance_matrix <- dist.ml(phy)
-
-        # assign IDs to each unique group of sequences
-        for (i in seq_along(region_sequences)) {
-            for (j in seq_len(i)) {
-                if (!is.na(distance_matrix[i][j]) && distance_matrix[i][j] == 0) {
-                    # TODO:: use rownames(distance_matrix) to create table with
-                    # names and IDs indicating which sequences match exactly
-                    print("exact match")
-                }
-            }
+        # separate out groups of identical sequences
+        alignment <- as(alignment, "DNAStringSet")
+        groups <- split(seq_along(alignment), as.character(alignment))
+        region_IDs <- list()
+        for (i in seq_along(groups)) {
+            ID <- paste0(region, i)
+            region_IDs[ID] <- lapply(groups[i], function(i) names(alignment)[i])
         }
-        # might need to convert alignment to DNAStringSet first
+        IDs <- c(IDs, region_IDs)
     }
+    print(IDs)
 }
 
 selectVRegions <- function() {
@@ -58,3 +54,23 @@ selectVRegions <- function() {
 # note: remember to always comment out scratch code you're using for tests
 # so the package will load correctly!
 alignVRegions(sequences)
+
+
+# This is still useful code, but a full distance calculation is more than
+# we need for now. I'm leaving this in here in case my implementation proves
+# inadequate when tested on actual data.
+# # use phangorn's ML-based distance calculation to determine
+# # which sequences are identical
+# phy <- phyDat(as.matrix(alignment), type = "DNA")
+# distance_matrix <- dist.ml(phy)
+
+# # assign IDs to each unique group of sequences
+# for (i in seq_along(region_sequences)) {
+#     for (j in seq_len(i)) {
+#         if (!is.na(distance_matrix[i][j]) && distance_matrix[i][j] == 0) {
+#             # TODO:: use rownames(distance_matrix) to create table with
+#             # names and IDs indicating which sequences match exactly
+#             print("exact match")
+#         }
+#     }
+# }
