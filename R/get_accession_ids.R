1a9ec006ca02daf6845ab9dca36ae79091394bf9
myeshagilliland <myeshagilliland@gmail.com>
2026-03-23 14:52:25 -0600

    new accession / org functions

--- R/get_accession_ids.R
+++ R/get_accession_ids.R
@@ -1,64 +1,36 @@
-#' get_accession_ids
-#'
-#' Given a list of species, outputs accession ids
-#'
-#' @param species_list list of valid species names
-#'
-#' @return list of accession ids (with copy numbers)
-#'
-#' @export
-#'
-#' @examples
-#' get_accession_ids(c("GCF_000006765.1.1", "GCF_000006765.1.2",
-#' "GCF_000006765.1.3", "GCF_000006765.1.4", "GCF_000007505.1.1",
-#' "GCF_000007505.1.2", "GCF_000007505.1.3"))
-#' [1] "Pseudomonas aeruginosa" "Pseudomonas aeruginosa" "Pseudomonas aeruginosa" "Pseudomonas aeruginosa"
-#' [5] "Brucella suis"          "Brucella suis"          "Brucella suis"
 
-get_accession_ids <- function(species_list) {
-  #import accessions data frame, columns: species_name, accession_id
-  acc_df <- get_accessions_df()
-  accession_ids <- c()
-
-  #for each species, find accessions id and add to accessions_ids
-  for (species in species_list) {
-    id <- acc_df$accession_id[acc_df$species_name == species]
-    accession_ids <- c(accession_ids, id)
-  }
-
-  return(accession_ids)
-}
-
-#' get_species_list
 #'
-#' Given a list of accession ids, outputs species list (with duplicates)
+#' get_accession_ids <- function(species_list) {
+#'   #import accessions data frame, columns: species_name, accession_id
+#'   acc_df <- get_accessions_df()
+#'   accession_ids <- c()
 #'
-#' @param accession_ids list of valid accession ids
+#'   #for each species, find accessions id and add to accessions_ids
+#'   for (species in species_list) {
+#'     id <- acc_df$accession_id[acc_df$species_name == species]
+#'     accession_ids <- c(accession_ids, id)
+#'   }
 #'
-#' @return list of species (with duplicates)
+#'   return(accession_ids)
+#' }
 #'
-#' @export
+
 #'
-#' @examples
-#' get_accession_ids(c("Pseudomonas aeruginosa", "Brucella suis"))
-#' [1] "GCF_000006765.1.1" "GCF_000006765.1.2" "GCF_000006765.1.3" "GCF_000006765.1.4"
-#' [5] "GCF_000007505.1.1" "GCF_000007505.1.2" "GCF_000007505.1.3"
-
-get_species_list <- function(accession_ids) {
-  #import accessions data frame, columns: species_name, accession_id
-  acc_df <- get_accessions_df()
-  species_list <- c()
-
-  #for each species, find accessions id and add to accessions_ids
-  for (id in accession_ids) {
-    species <- acc_df$species_name[acc_df$accession_id == id]
-    species_list <- c(species_list, species)
-  }
-
-  return(species_list)
-}
-
-# print(get_accession_ids(c("Pseudomonas aeruginosa", "Brucella suis")))
-
-# print(get_species_list(c("GCF_000006765.1.1", "GCF_000006765.1.2", "GCF_000006765.1.3", "GCF_000006765.1.4", "GCF_000007505.1.1", "GCF_000007505.1.2", "GCF_000007505.1.3")))
-
+#' get_species_list <- function(accession_ids) {
+#'   #import accessions data frame, columns: species_name, accession_id
+#'   acc_df <- get_accessions_df()
+#'   species_list <- c()
+#'
+#'   #for each species, find accessions id and add to accessions_ids
+#'   for (id in accession_ids) {
+#'     species <- acc_df$species_name[acc_df$accession_id == id]
+#'     species_list <- c(species_list, species)
+#'   }
+#'
+#'   return(species_list)
+#' }
+#'
+#' # print(get_accession_ids(c("Pseudomonas aeruginosa", "Brucella suis")))
+#'
+#' # print(get_species_list(c("GCF_000006765.1.1", "GCF_000006765.1.2", "GCF_000006765.1.3", "GCF_000006765.1.4", "GCF_000007505.1.1", "GCF_000007505.1.2", "GCF_000007505.1.3")))
+#'
--- R/ids_to_organisms.R
+++ R/ids_to_organisms.R
@@ -1,68 +1,109 @@
-# # library(stringi)
-# # library(data.table)
-#
+library(stringi)
+library(data.table)
+
 # accessions_loaded <- FALSE
-#
-# load_accessions <- function() {
-#   #get path to RADlib
-#   file_path <- system.file("extdata", "RADlib16S.fa", package = "RADalign")
-#   lines <- readLines(file_path)
-#
-#   #get
-#   index <- Biostrings::fasta.index(file_path, seqtype = "DNA")
-#   # print(index[desc])
-#   headers <- index[["desc"]] # remember to change this code for download functions now that header has changed
-#
-#   # get data from headers
-#   labels <- stri_split_fixed(headers, "=", simplify = TRUE)
-#   gene_id <- stri_split_fixed(labels[,1], " ", simplify = TRUE)[,1]
-#   taxa_id <- stri_split_fixed(labels[,2], " ", simplify = TRUE)[,1]
-#   organism_name <- stri_split_fixed(labels[,3], "\"", simplify = TRUE)[,2]
-#
-#   #create empty accessions
-#   n <- length(headers)
-#   accessions <- data.table(id = character(n), taxid = character(n), organism = character(n))
-#
-#   #fill accessions with memory pointers
-#   accessions[, id := gene_id]
-#   accessions[, taxid := taxa_id]
-#   accessions[, organism := organism_name]
-#
-#   return (accessions)
-#
-# }
-#
-# get_organisms <- function(ids) {
-#
-#   # if (accessions_loaded == FALSE) {
-#   #   accessions <- load_accessions()
-#   #   accessions_loaded <- TRUE
-#   # }
-#
-#   accessions <- load_accessions()
-#   organisms <- accessions[id %in% ids, organism]
-#
-#   return (organisms)
-# }
-#
-# get_ids <- function(organisms) {
-#
-#   accessions <- load_accessions()
-#   organisms <- accessions[organism %in% organisms, id]
-#
-#   return (organisms)
-# }
-#
-# # get_ids <- function(organisms) {
-# # }
-#
+
+#' get_accessions_df
+#'
+#' This function generates a callable df with two columns: species_name and accession_id
+#'
+#' @return This function returns a callable df with two columns: species_name and accession_id
+#'
+#' @export
+#'
+#' @examples
+#' get_accessions_df()
+#' > species_name accession_id
+
+get_accessions_df <- function() {
+  #get path to RADlib
+  file_path <- system.file("extdata", "RADlib16S.fa", package = "RADalign")
+  lines <- readLines(file_path)
+
+  #get
+  index <- Biostrings::fasta.index(file_path, seqtype = "DNA")
+  # print(index[desc])
+  headers <- index[["desc"]] # remember to change this code for download functions now that header has changed
+
+  # get data from headers
+  labels <- stri_split_fixed(headers, "=", simplify = TRUE)
+  gene_id <- stri_split_fixed(labels[,1], " ", simplify = TRUE)[,1]
+  taxa_id <- stri_split_fixed(labels[,2], " ", simplify = TRUE)[,1]
+  organism_name <- stri_split_fixed(labels[,3], "\"", simplify = TRUE)[,2]
+
+  #create empty accessions
+  n <- length(headers)
+  accessions <- data.table(id = character(n), taxid = character(n), organism = character(n))
+
+  #fill accessions with memory pointers
+  accessions[, id := gene_id]
+  accessions[, taxid := taxa_id]
+  accessions[, organism := organism_name]
+
+  return (accessions)
+
+}
+
+#' get_species_list
+#'
+#' Given a list of accession ids, outputs species list (with duplicates)
+#'
+#' @param accession_ids list of valid accession ids
+#'
+#' @return list of species (with duplicates)
+#'
+#' @export
+#'
+#' @examples
+#' get_accession_ids(c("Pseudomonas aeruginosa", "Brucella suis"))
+#' [1] "GCF_000006765.1.1" "GCF_000006765.1.2" "GCF_000006765.1.3" "GCF_000006765.1.4"
+#' [5] "GCF_000007505.1.1" "GCF_000007505.1.2" "GCF_000007505.1.3"
+
+get_species_list <- function(ids) {
+
+  # if (accessions_loaded == FALSE) {
+  #   accessions <- load_accessions()
+  #   accessions_loaded <- TRUE
+  # }
+
+  accessions <- get_accessions_df()
+  organisms <- accessions[id %in% ids, organism]
+
+  return (organisms)
+}
+
+#' get_accession_ids
+#'
+#' Given a list of species, outputs accession ids
+#'
+#' @param species_list list of valid species names
+#'
+#' @return list of accession ids (with copy numbers)
+#'
+#' @export
+#'
+#' @examples
+#' get_accession_ids(c("GCF_000006765.1.1", "GCF_000006765.1.2",
+#' "GCF_000006765.1.3", "GCF_000006765.1.4", "GCF_000007505.1.1",
+#' "GCF_000007505.1.2", "GCF_000007505.1.3"))
+#' [1] "Pseudomonas aeruginosa" "Pseudomonas aeruginosa" "Pseudomonas aeruginosa" "Pseudomonas aeruginosa"
+#' [5] "Brucella suis"          "Brucella suis"          "Brucella suis"
+
+get_accession_ids <- function(organisms) {
+
+  accessions <- get_accessions_df()
+  organisms <- accessions[organism %in% organisms, id]
+
+  return (organisms)
+}
+
 # id_list <- c("AB6B37_RS01935", "THECO_RS17145", "FW767_RS11870")
 # org_list <- c("Fretibacter rubidus", "Thermobacillus composti KWC4", "Heminiphilus faecis")
 #
 # print(get_organisms(id_list))
 # print(get_ids(org_list))
-#
-# # AB6B37_RS01935 taxid=570162 organism="Fretibacter rubidus"
-# # THECO_RS17145 taxid=717605 organism="Thermobacillus composti KWC4"
-# # FW767_RS11870 taxid=2601703 organism="Heminiphilus faecis"
-#
+
+# AB6B37_RS01935 taxid=570162 organism="Fretibacter rubidus"
+# THECO_RS17145 taxid=717605 organism="Thermobacillus composti KWC4"
+# FW767_RS11870 taxid=2601703 organism="Heminiphilus faecis"
+
