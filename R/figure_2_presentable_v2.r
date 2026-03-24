47343399713694824c9e82a978e58c169b971568
Remi <90064274+Remi-Neal@users.noreply.github.com>
2026-03-24 14:14:07 -0600

    Updated ids_to_organims.R to use double colon syntax

--- R/figure_2_presentable_v2.r
+++ R/figure_2_presentable_v2.r
@@ -13,6 +13,30 @@
 # fraction of each species colored? May be too busy
 # 
 
+DATADIR <- "..."
+SILVA_URL <- "https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz"
+GREENGENE_URL <- "https://ftp.microbio.me/greengenes_release/current/2024.09.taxonomy.id.tsv.gz"
+NCBI_URL <- "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.16SrRNA.fna.gz"
+RADLIB_URL <- "https://zenodo.org/records/19102712/files/RADlib16S.fa?download=1"
+
+# ---- Download Data ----
+silva_conn <- RCurl::getURL(SILVA_URL)
+greengene_conn <- RCurl::getURL(GREENGENE_URL)
+ncbi_conn <- RCurl::getURL(NCBI_URL)
+radlib_conn <- RCurl::getURL(RADLIB_URL)
+
+# TODO: replace read.data with function that actually reads the data (fasta etc.)
+# TODO: All db are probably gzipped so need step to use gzfile() to unzip data
+# silva_data <- read.data(testConnection(silva_conn)) # Read Fasta
+# greengeen_data <- read.data(testConnection(greengene_conn), sep='\t') # This is like 2+ gigs so try to only download once
+# ncbi_data <- read.data(testConnection(ncbi_conn))
+# radlib_data <- read.data(testConnection(radlib_conn))
+
+# ---- Create Dataframes ----
+
+# ---- Combine Dataframes ----
+
+# ---- GGPlot ----
 
 # <- <- <- <- Old Stuff -> -> -> ->
 # library(rentrez)
