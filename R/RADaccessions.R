560324b9d3bdb08c76d1c0112a661b905a502998
myeshagilliland <myeshagilliland@gmail.com>
2026-04-01 23:12:29 -0600

    updated comments, slight order adjustments, and slight formatting in headers

--- R/RADaccessions.R
+++ R/RADaccessions.R
@@ -2,14 +2,14 @@
 #'
 #' This function generates a metadata table from the header data of RADlib.
 #'
-#' @return A data.frame object, with <char> type columns id, taxid, organism, and genus.
+#' @return <data.frame> table with <char> columns: id, taxid, organism, and genus.
 #'
 #' @import data.table
 #'
 #' @export
 #'
 #' @examples
-#' get_accessions_df()
+#' > get_accessions_df()
 #'                id   taxid               organism               genus
 #'            <char>  <char>                 <char>              <char>
 #' 1: AB6B37_RS01935  570162    Fretibacter rubidus         Fretibacter
@@ -17,64 +17,59 @@
 #' 3:  KK102_RS22065 2838947 Enterobacter quasimori        Enterobacter
 
 get_accessions_df <- function() {
-  #get path to RADlib
+
+  # get path to RADlib
   file_path <- system.file("extdata", "RADlib16S.fa", package = "RADalign")
   lines <- readLines(file_path)
 
-  #get all headers from RADlib
+  # get all headers from RADlib
   index <- Biostrings::fasta.index(file_path, seqtype = "DNA")
-  headers <- index[["desc"]] # remember to change this code for download functions now that header has changed
+  headers <- index[["desc"]]
 
   # get data from headers
-  # labels <- stringi::stri_split_fixed(headers, "=", simplify = TRUE)
-  # gene_id <- stringi::stri_split_fixed(labels[,1], " ", simplify = TRUE)[,1]
   gene_id <- stringi::stri_match_all_regex(headers, "^\\w+") |>
     unlist()
-  # taxa_id <- stringi::stri_split_fixed(labels[,2], " ", simplify = TRUE)[,1]
   taxa_id <- stringi::stri_match_all_regex(headers, "taxid=(\\d+)") |>
     lapply(function(x) x[[2]]) |>
     unlist()
-  # organism_name <- stringi::stri_split_fixed(labels[,3], "\"", simplify = TRUE)[,2]
   organism_name <- stringi::stri_match_all_regex(headers, "organism=\"(.*)\"") |>
     lapply(function(x) x[[2]]) |>
     unlist()
-  # genus_name <- stringi::stri_split_fixed(organism_name, " ", simplify = TRUE)[,1]
+  # selects the first word of an organism name, ignores leading non-word characters
   genus_name <- stringi::stri_match_first_regex(organism_name, "(?!\\W)(\\w+)") |>
-    (function(x) x[,2])() # Selects the first word that's part of an organisms name ignoring leading non-word characters
+    (function(x) x[,2])()
 
-  #create empty accessions
+  # create empty accessions
   n <- length(headers)
   accessions <- data.table::data.table(id = character(n),
                                        taxid = character(n),
                                        organism = character(n),
-                                       header = character(n))
+                                       genus = character(n))
 
-  #fill accessions with memory pointers
+  # fill accessions with memory pointers
   accessions[, id := gene_id]
   accessions[, taxid := taxa_id]
   accessions[, organism := organism_name]
   accessions[, genus := genus_name]
-  accessions[, header := headers]
 
-  #filter out unnamed RADlib entries
+  # filter out unnamed RADlib entries
   accessions <- accessions[!is.na(organism) & organism != ""]
 
   return (accessions)
-
 }
 
 #' get_species_list
 #'
-#' Given a list of accession ids, outputs organisms list (includes duplicates)
+#' Given a list of accession ids, outputs corresponding organism names (includes duplicates)
 #'
-#' @param accession_ids list of valid accession ids
+#' @param accession_ids <char list> of valid accession ids
 #'
-#' @return list of organisms (includes duplicates)
+#' @return <char list> of organism names (includes duplicates)
 #'
 #' @export
 #'
 #' @examples
-#' get_species_list(c("EDX97_RS04345", "EDX97_RS05225", "EDX97_RS06840",
+#' > get_species_list(c("EDX97_RS04345", "EDX97_RS05225", "EDX97_RS06840",
 #' "EDX97_RS09045", "EDX97_RS10020", "EDX97_RS11935", "MOZ64_RS11590"))
 #' [1] "Absicoccus porci"        "Absicoccus porci"        "Absicoccus porci"
 #' [4] "Absicoccus porci"       "Absicoccus porci"        "Absicoccus porci"
@@ -82,35 +77,26 @@ get_accessions_df <- function() {
 
 get_species_list <- function(ids) {
 
-  # if (accessions_loaded == FALSE) {
-  #   accessions <- load_accessions()
-  #   accessions_loaded <- TRUE
-  # }
-
   accessions <- get_accessions_df()
-  # organisms <- accessions[id %in% ids, organism]
-  # organisms <- accessions[.(ids), on = .(id), organism]
 
-  # return (organisms)
-
+  # filter accessions by ids
   organisms <- accessions[.(ids), on = .(id), .(organism, id)]
-  # print(organisms[is.na(organism)]$id)
 
   return (organisms[!is.na(organism)]$organism)
 }
 
 #' get_accession_ids
 #'
-#' Given a list of organisms, outputs accession ids
+#' Given a list of organism names, outputs all corresponding accession ids
 #'
-#' @param organisms list of valid organism names
+#' @param organisms <char list> of valid organism names
 #'
-#' @return list of accession ids (with copy numbers)
+#' @return <char list> of accession ids
 #'
 #' @export
 #'
 #' @examples
-#' get_accession_ids(c("Absicoccus porci", "Absicoccus intestinalis"))
+#' > get_accession_ids(c("Absicoccus porci", "Absicoccus intestinalis"))
 #' "GCF_000007505.1.2", "GCF_000007505.1.3"))
 #' [1] "EDX97_RS04345" "EDX97_RS05225" "EDX97_RS06840" "EDX97_RS09045"
 #' [5] "EDX97_RS10020" "EDX97_RS11935" "MOZ64_RS11590"
@@ -118,21 +104,18 @@ get_species_list <- function(ids) {
 get_accession_ids <- function(organisms) {
 
   accessions <- get_accessions_df()
-  # ids <- accessions[organism %in% organisms, id]
-  # print(ids)
-  # ids <- accessions[.(organisms), on = .(organism), id]
-  # return (ids)
+
+  # filter accessions by organisms
   ids <- accessions[.(organisms), on = .(organism), .(organism, id)]
-  print(ids[is.na(id)]$organism)
 
   return (ids[!is.na(id)]$id)
 }
 
 #' get_all_organisms
 #'
-#' outputs all unique organisms in RADlib
+#' Generates a sorted list of all unique organisms in RADlib and adds genus labels ("Genus - All Species (#)").
 #'
-#' @return list of organism names
+#' @return <char list> of organism names (and genus labels)
 #'
 #' @export
 #'
@@ -142,17 +125,22 @@ get_accession_ids <- function(organisms) {
 #' [4] "Abiotrophia defectiva"             "Absicoccus - All Species (2)"      "Absicoccus intestinalis"
 
 get_all_organisms <- function() {
+
   accessions <- get_accessions_df()
+
+  # get unique organisms and genera in accessions
   organism_list <- unique(accessions$organism)
   genus_list <- unique(accessions$genus)
 
+  # calculate number of species per genus
   genus_counts <- accessions |>
     dplyr::distinct(genus, organism) |>
     dplyr::count(genus, name = "n")
 
-  # genus_labels <- paste0(genus_list, " - All Species")
+  # build genus label with format "Genus - All Species (#)"
   genus_labels <- paste0(genus_counts$genus, " - All Species (", genus_counts$n, ")")
 
+  # combine organism and genus label lists and sort
   full_list <- stringi::stri_sort(append(genus_labels, organism_list))
 
   return(full_list)
@@ -160,9 +148,11 @@ get_all_organisms <- function() {
 
 #' get_species_from_genus
 #'
-#' outputs all organisms in RADlib of a given genus
+#' Finds all organisms in RADlib of a given genus
 #'
-#' @return list of organism names
+#' @param genus_label <char> genus label with format "Genus - All Species (#)"
+#'
+#' @return <char list> of organism names
 #'
 #' @export
 #'
@@ -171,12 +161,13 @@ get_all_organisms <- function() {
 #' [1] "Absicoccus porci"        "Absicoccus intestinalis"
 
 get_species_from_genus <- function(genus_label) {
+
   accessions <- get_accessions_df()
 
-  #get genus name from input string
+  # get genus name from input string
   genus_name <- stringi::stri_split_fixed(genus_label, " - ", simplify = TRUE)[,1]
 
-  #return all organisms with matching genus
+  # return all organisms with matching genus
   species <- unique(accessions[genus %in% genus_name]$organism)
 
   return (unique(species))
--- R/RADdownload.R
+++ R/RADdownload.R
@@ -4,6 +4,7 @@
 #'
 #' @param pipeline <char> name of pipeline, valid inputs: "MetaScope"
 #' @param organisms_list <char list> of organism names to download from RADlib
+#' @param filter <boolean> (optional) for filter/reference sequence download, defaults to FALSE
 #' @param download_location <char> (optional) file path to desired download location, defaults to "Downloads" folder
 #'
 #' @return <char> path to downloaded folder of files
@@ -16,38 +17,45 @@
 
 download_RAD_data <- function(pipeline, organisms_list, filter = FALSE, download_location = fs::path_home("Downloads")) {
 
-  #generate unique folder name
+  # generate unique folder name
   rand_string <- paste0(sample(c(letters, LETTERS, 0:9), 8, replace = TRUE), collapse = "")
   folder_name <- paste0("RADdownloads", format(Sys.time(), "_%d%m%Y_%H%M%S_"), rand_string)
 
-  #create path for folder
+  # create path for folder
   download_folder <- file.path(download_location, folder_name)
   if (!dir.exists(download_folder)) {
     dir.create(download_folder, recursive = TRUE)
   }
 
-  #download necessary files for selected pipeline
+  # download necessary files for selected pipeline
   if (pipeline == "MetaScope") {
+
     if (filter == FALSE) {
 
-      #get accession ids for all organisms in organisms_list
+      # get accession ids for all organisms in organisms_list
       accessions_list <- get_accession_ids(organisms_list)
-      #generate reference sequence files & save folder name
+
+      # generate reference sequence files & save folder name
       reference_folder <- download_MetaScope_reference(accessions_list, download_folder)
 
       return(file.path(download_folder, reference_folder$folder))
 
     } else if (filter == TRUE) {
 
-      #get accession ids for all organisms in organisms_list
+      # get accession ids for all organisms in organisms_list
       accessions_list <- get_accession_ids(organisms_list)
-      #generate filter sequence files & save folder name
+
+      # generate filter sequence files & save folder name
       filter_folder <- download_MetaScope_reference(accessions_list, download_folder, TRUE)
 
       return(file.path(download_folder, filter_folder$folder))
     }
+
   } else {
+
+    # print warning message to console
     cat(paste0(pipeline, " integration currently unsupported"))
+
     return("")
   }
 }
@@ -56,7 +64,7 @@ download_RAD_data <- function(pipeline, organisms_list, filter = FALSE, download
 #'
 #' This function downloads selected sequences from RADlib to a desired folder as fasta files and places them inside a single sub folder.
 #'
-#' @param accessions_list <list> of string accession ids of sequences to download from RADlib
+#' @param accessions_list <char list> of accession ids of sequences to download from RADlib
 #' @param download_folder <char> path to desired destination folder (sub folder will be created)
 #' @param filter <boolean> (optional) for filter/reference sequence download, defaults to FALSE
 #'
@@ -80,38 +88,40 @@ download_MetaScope_reference <- function(accessions_list, download_folder, filte
 
   organisms_list <- get_organisms_list(accessions_list)
 
-  #folder for sequence fastas
+  # get file path to RADlib
+  RADlib_path <- system.file("extdata", "RADlib16S.fa", package = "RADalign")
+
+  # return selected sequences from RADlib
+  sequences <- readSequences(RADlib_path, accessions_list)
+
+  # determine sub folder name
   if (!filter) {
     folder_name <- "MetaScope_reference_dir"
   } else {
     folder_name <- "MetaScope_filter_dir"
   }
+
+  # create sub folder
   folder_path = file.path(download_folder, folder_name)
   if (!dir.exists(folder_path)) {
     dir.create(folder_path, recursive = TRUE)
   }
 
-  #get file path to RADlib
-  RADlib_path <- system.file("extdata", "RADlib16S.fa", package = "RADalign")
-
-  #return selected sequences from RADlib
-  sequences <- readSequences(RADlib_path, accessions_list)
-
-  #download one fasta file per sequence
+  # download one fasta file per sequence
   file_names <- vector("list", length(sequences))
   for (i in seq_along(sequences)) {
+
     id <- accessions_list[i]
 
-    #generate file name
+    # generate file name
     organisms_name <- paste(unlist(strsplit(organisms_list[i], " ")), collapse = "_")
     seq_file_name <- paste0(organisms_name, "_", id, ".fasta")
     seq_file_path <- file.path(folder_path, seq_file_name)
 
-    #write fasta file for sequence, and save name
+    # write fasta file for sequence and save file name
     Biostrings::writeXStringSet(sequences[i], seq_file_path)
     file_names[[i]] <- seq_file_name
   }
 
   return (list(folder = folder_name, files = file_names))
-
 }
